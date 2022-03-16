6cb8584ac54f7451018ae5873f606bbdb37fee01
abbeygorny <97626142+abbeygorny@users.noreply.github.com>
2022-03-16 18:13:35 -0400

    progress

--- Levels/Camera.gd
+++ Levels/Camera.gd
@@ -6,8 +6,7 @@ func _ready():
 	Global.death_zone = limit_bottom + 200
 
 func _process(_delta):
-	if player == null:
-		player = get_node("/root/Game/Player_Container/Player")
+	player = get_node("/root/Game/Player_Container/Player")
 	if player != null:
 		position = player.position
 
@@ -15,3 +14,4 @@ func _physics_process(_delta):
 	var vtrans = get_canvas_transform()
 	var top_left = -vtrans.get_origin() / vtrans.get_scale()
 	$Fade.rect_global_position = top_left
+	
